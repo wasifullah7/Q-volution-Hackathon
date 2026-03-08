261e217bce837359cc60872b7403b4840b672adb
wasifullah7 <wasif.wwez@gmail.com>
2026-03-08 14:43:42 +0500

    add the port at the vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
